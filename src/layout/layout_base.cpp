85d48bb292a63a8915f13a4196dd1afffff5997a
Ivan Malison <IvanMalison@gmail.com>
2026-07-20 09:22:58 -0700

    Support Hyprland 0.56

--- src/layout/layout_base.cpp
+++ src/layout/layout_base.cpp
@@ -12,6 +12,7 @@
 #include <hyprland/src/render/pass/ClearPassElement.hpp>
 #include <hyprland/src/render/pass/RectPassElement.hpp>
 #include <hyprland/src/render/pass/TexPassElement.hpp>
+#include <hyprland/src/state/WorkspaceState.hpp>
 #undef private
 
 #include "../config.hpp"
@@ -234,7 +235,7 @@ CBox HTLayoutBase::get_global_window_box(PHLWINDOW window, WORKSPACEID workspace
     if (monitor == nullptr)
         return {};
 
-    const PHLWORKSPACE workspace = g_pCompositor->getWorkspaceByID(workspace_id);
+    const PHLWORKSPACE workspace = State::workspaceState()->query().id(workspace_id).run();
     if (workspace == nullptr || workspace->m_monitor != monitor)
         return {};
 
